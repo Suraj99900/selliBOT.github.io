08d8fe511deb4caa78c687d24a20c2dc69777b65
Suraj_SJ <62930129+Suraj99900@users.noreply.github.com>
2022-10-03 16:39:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+    <title>selli my chat bot</title>
+</head>
+
+<body>
+
+    <!-- header for local chat bot selli -->
+
+    <div class="alert alert-primary" role="alert">
+        <div class="header_bot">
+            <img src="img/icons8-girl-and-check-100.png" alt="selli img">
+            <h2>Hi My Name is Selli </h2>
+        </div>
+        <p>I could never get bored talking to you :)</p>
+    </div>
+
+    <div class="chatbox" id="chatbot">
+
+        
+    </div>
+
+    <div class="msg">
+
+        <div class="msg_type">
+            <input type="text" name="msg" id="usermsg" placeholder="enter your msg">
+        </div>
+
+        <button class="btn btn-primary" id="send">
+            send
+        </button>
+
+
+    </div>
+
+
+
+    <script src="main.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ main.js
@@ -0,0 +1,63 @@
+let send = document.getElementById('send');
+let msg = document.getElementById('usermsg');
+let chat =document.getElementById('chatbot');
+// request to selli for answer to msg
+var i=0;
+function requestSelli() {
+  
+    if (msg.value != "") {
+        
+        var user = document.createElement('div');
+        var div_box_user = document.createElement('div');
+        div_box_user.setAttribute('class','usermsg')
+        div_box_user.setAttribute('id','u'+i);
+        var div_box_selli = document.createElement('div');
+        user.setAttribute('class', 'alert alert-primary user_msg');
+        user.innerHTML = "<img src='img/icons8-user-100.png' alt='user img' style='width:50px;'>" + msg.value;
+        div_box_user.append(user);
+        chat.append(div_box_user);
+
+        var selli = document.createElement('div');
+        selli.setAttribute('class', 'alert alert-info selli_msg');
+
+
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET", "req.php?req=" + msg.value, true);
+
+        ajax.onload = () => {
+            div_box_selli.setAttribute('id','s'+i);
+            div_box_selli.setAttribute('class','sellimsg');
+            var selliMsg = ajax.responseText;
+            if (selliMsg != "") {
+
+                selli.innerHTML = "<img src='img/icons8-girl-and-check-50.png' alt='selli img'>"+selliMsg;
+               
+                console.log(selliMsg);
+               
+                div_box_selli.append(selli);
+                chat.append(div_box_selli);
+                scrollDown();
+                i++;
+            }
+        }
+        ajax.send();
+
+        
+    }
+
+}
+
+function scrollDown(){
+    document.getElementById('chatbot').scrollTo(0,document.getElementById('chatbot').scrollHeight)
+}
+
+
+
+
+
+
+
+
+
+
+send.addEventListener('click', requestSelli);
